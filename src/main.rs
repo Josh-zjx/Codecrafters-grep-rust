e06b1a3aff7aacab4c452d38e67f55082aed7b5e
Jiaxu Zhu <josh.zhujiaxu@gmail.com>
2024-08-28 15:59:20 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
+    } else if pattern == r"\d" {
+        for i in input_line.chars() {
+            if i.is_numeric() {
+                return true;
+            }
+        }
+        return false;
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
